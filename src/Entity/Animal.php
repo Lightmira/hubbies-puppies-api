7e949584282f61f4b38dac5cd28f551c944edab8
ecrespi <etienne.crespi@laposte.net>
2020-05-11 13:05:13 +0200

    Feature/animal entity (#2)
    * [Add] animal entity
    
    * [Update] replace symfony/serializer ==> jms/serializer

---
+++ src/Entity/Animal.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=AnimalRepository::class)
+ */
+class Animal
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Column(name="uuid", type="uuid", unique=true)
+     * @Serializer\SerializedName("id")
+     * @Serializer\Type("uuid")
+     */
+    private $uuid;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Serializer\SerializedName("name")
+     *
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="gender", type="string", columnDefinition="enum('male', 'femele')")
+     * @Serializer\SerializedName("gender")
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(name="age", type="integer")
+     * @Serializer\SerializedName("age")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(name="description", type="text", nullable=true)
+     * @Serializer\SerializedName("description")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
